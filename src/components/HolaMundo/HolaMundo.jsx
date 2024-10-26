c76967761613136b982e498b7288fc424f05a8e4
LilAngelBruh <angel117tutu@gmail.com>
2024-10-26 10:40:56 -0700

    Reinicio de proyecto

--- src/components/HolaMundo/HolaMundo.jsx
+++
@@ -1,9 +0,0 @@
-import './holaMundo.css';
-
-export default function HolaMundo() {
-  return (
-    <div>
-      <h1 className='holaMundo'>Hola Mundo</h1>
-    </div>
-  );
-}
--- src/pages/MiPagina/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header';
-export { default as Section1 } from './Section1';
-export { default as Section2 } from './Section2';
