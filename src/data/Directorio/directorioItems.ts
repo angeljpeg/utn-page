01f999eec137f2f987ca8f79a6bb9a6ca4d77f51
angel_less <angel117tutu@gmail.com>
2024-11-26 23:56:19 -0700

    direcotiro data

---
+++ src/data/Directorio/directorioItems.ts
@@ -0,0 +1,172 @@
+// RECTOR
+import Imagen1 from '@/assets/img/directorioImagenes/rector/1.webp'
+// DIR. CARRERA
+import Imagen3 from '@/assets/img/directorioImagenes/directoresCarrera/3.webp'
+import Imagen4 from '@/assets/img/directorioImagenes/directoresCarrera/4.webp'
+import Imagen2 from '@/assets/img/directorioImagenes/directoresCarrera/2.webp'
+import Imagen5 from '@/assets/img/directorioImagenes/directoresCarrera/5.webp'
+// DIR. AREA
+import Imagen6 from '@/assets/img/directorioImagenes/directoresArea/6.webp'
+import Imagen7 from '@/assets/img/directorioImagenes/directoresArea/7.webp'
+import Imagen8 from '@/assets/img/directorioImagenes/directoresArea/8.webp'
+import Imagen9 from '@/assets/img/directorioImagenes/directoresArea/9.webp'
+import Imagen10 from '@/assets/img/directorioImagenes/directoresArea/10.webp'
+import Imagen11 from '@/assets/img/directorioImagenes/directoresArea/11.webp'
+// JFS. DEPARTAMENTO
+import Imagen12 from '@/assets/img/directorioImagenes/jefesDepartamento/12.webp'
+import Imagen13 from '@/assets/img/directorioImagenes/jefesDepartamento/13.webp'
+import Imagen14 from '@/assets/img/directorioImagenes/jefesDepartamento/14.webp'
+import Imagen15 from '@/assets/img/directorioImagenes/jefesDepartamento/15.webp'
+import Imagen16 from '@/assets/img/directorioImagenes/jefesDepartamento/16.webp'
+import Imagen17 from '@/assets/img/directorioImagenes/jefesDepartamento/17.webp'
+import Imagen18 from '@/assets/img/directorioImagenes/jefesDepartamento/18.webp'
+import Imagen19 from '@/assets/img/directorioImagenes/jefesDepartamento/19.webp'
+import Imagen20 from '@/assets/img/directorioImagenes/jefesDepartamento/20.webp'
+import Imagen21 from '@/assets/img/directorioImagenes/jefesDepartamento/21.webp'
+import Imagen22 from '@/assets/img/directorioImagenes/jefesDepartamento/22.webp'
+import Imagen23 from '@/assets/img/directorioImagenes/jefesDepartamento/23.webp'
+import Imagen24 from '@/assets/img/directorioImagenes/jefesDepartamento/24.webp'
+import Imagen25 from '@/assets/img/directorioImagenes/jefesDepartamento/25.webp'
+import Imagen26 from '@/assets/img/directorioImagenes/jefesDepartamento/26.webp'
+
+export const rector = [
+  {
+    nombre: "Mtro. Cuauhtémoc Martínez Siraitare",
+    ocupacion: "Rector",
+    imagen: Imagen1
+  }
+]
+
+export const directoresCarrera = [
+  {
+    nombre: "Dr. René Castro Morales",
+    ocupacion: "OCI CADA y Desarrollo de Negocios",
+    imagen: Imagen2
+  },
+  {
+    nombre: "MC Nora Anabel López Aguirre",
+    ocupacion: "Energías Renovables & Mantenimiento Indistrial",
+    imagen: Imagen3
+  },
+  {
+    nombre: "MA Mario Javier Nieblas Nuñez",
+    ocupacion: "Sistemas Productivos & Manufactura Aeronáutica",
+    imagen: Imagen4
+  },
+  {
+    nombre: "Ing. Edgar Alonso Sosa Camargo",
+    ocupacion: "Tecnológias de la Informacion & Mecatronica",
+    imagen: Imagen5
+  }
+]
+
+export const directoresArea = [
+  {
+    nombre: "M.E. Gilberto García Rocha",
+    ocupacion: "Director de Administración y Finanzas",
+    imagen: Imagen6
+  },
+  {
+    nombre: "Lic. Juan Liborio Velázquez",
+    ocupacion: "Director Jurídico",
+    imagen: Imagen7
+  },
+  {
+    nombre: "Lic. Leticia Guadalupe Gattas Suarez",
+    ocupacion: "Directora de Planeación",
+    imagen: Imagen8
+  },
+  {
+    nombre: "Lic. Jesús Manuel López Terán",
+    ocupacion: "Director de Extensión Universitaria",
+    imagen: Imagen9
+  },
+  {
+    nombre: "Ing. Laura Elena López Félix",
+    ocupacion: "Directora de Apoyo Educativo",
+    imagen: Imagen10
+  },
+  {
+    nombre: "Ing. Karla Leticia Rivera Nieblas",
+    ocupacion: "Directora de Vinculación",
+    imagen: Imagen11
+  },
+]
+
+export const jefesDepartamento = [
+  {
+    nombre: "C.P. Gema Vanessa Loroña López",
+    ocupacion: "Contabilidad y Finanzas",
+    imagen: Imagen12
+  },
+  {
+    nombre: "MDN Karenina Mak Huerta",
+    ocupacion: "Planeación y Evaluación",
+    imagen: Imagen13
+  },
+  {
+    nombre: "MA Moserrat Borbón Egurrola",
+    ocupacion: "Información y Estadística",
+    imagen: Imagen14
+  },
+  {
+    nombre: "Lic. José Valentino Rodríguez Hernández",
+    ocupacion: "Investigación y Desarrollo",
+    imagen: Imagen15
+  },
+  {
+    nombre: "CFP Beatríz Adriana Cota Rivera",
+    ocupacion: "Programación y Presupuesto",
+    imagen: Imagen16
+  },
+  {
+    nombre: "Lic. Jesús Ramón Mancilla García",
+    ocupacion: "Servicios Estudiantiles",
+    imagen: Imagen17
+  },
+  {
+    nombre: "Lic. Claudia Verónica López Portugal",
+    ocupacion: "Recursos Humanos",
+    imagen: Imagen18
+  },
+  {
+    nombre: "MA Herminia Aldaz Ruvalcaba",
+    ocupacion: "Prensa y Difusión",
+    imagen: Imagen19
+  },
+  {
+    nombre: "Lic. Lorenia Rodríguez García",
+    ocupacion: "Actividades Culturales y Deportivas",
+    imagen: Imagen20
+  },
+  {
+    nombre: "C. María de la Luz Carrazco Reyes",
+    ocupacion: "Servicios Bibliotecarios",
+    imagen: Imagen21
+  },
+  {
+    nombre: "Lic. Miguel Angel Pacheco",
+    ocupacion: "Recursos Materiales",
+    imagen: Imagen22
+  },
+  {
+    nombre: "Dr. Roberto Rodriguez Maldonado",
+    ocupacion: "Servicios Médicos",
+    imagen: Imagen23
+  },
+  {
+    nombre: "C. Alma Luz Oliva Gámez",
+    ocupacion: "Mantenimiento e Instalaciones",
+    imagen: Imagen24
+  },
+  {
+    nombre: "Lic. Jorge Jiménez Briseño",
+    ocupacion: "Prácticas y Estadías",
+    imagen: Imagen25
+  },
+  {
+    nombre: "Lic. María Luisa Arvizu Marrufo",
+    ocupacion: "Servicios Escolares",
+    imagen: Imagen26
+  }
+]
